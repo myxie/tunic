3e893b216e3b93e44b5517b6f0d623f1903bc674
sergeantpepper <rwbunney@gmail.com>
2016-08-15 22:09:23 +0800

    Added basic macros for unit testing

---
+++ ctest.h
@@ -0,0 +1,10 @@
+/*Code initially developed from http://www.jera.com/techinfo/jtns/jtn002.html*/
+
+#define ctest_assert(message, test)
+    do{ if(!(test)) {return message;} } while(0)
+
+#define ctest_run_test(test)
+    do{ char *message = test(); tests_run++; \
+        if (message) return message; } while(0) 
+
+extern int tests_run;
