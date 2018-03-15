c2a87ed54f5efe1da44d3cac5e0b734d1847f5a6
croutons <21298244@uwa.edu.edu.au>
2018-03-15 16:43:34 +0800

    Build file and run_test_suite macro

--- mtest.h
+++ mtest.h
@@ -18,6 +18,12 @@
 #include <time.h>
 #endif
 
+enum MTEST_output_level{
+	BASIC_RESULTS,
+	TIME,
+	ERRORS_ONLY
+};
+
 
 int tests_run;
 int tests_passed;
@@ -44,22 +50,31 @@ void test_atoi_function(){
 
 
 int main(){
-	m_run_tests(MTEST_Output_Detail_Value, test_atoi_function());
-
+	m_run_tests();
 	etc....
+
 }
 */
+// Run timing functions here
+#define MTEST_run_test_suite(test_suite_name, mtest_output_level) do {\
+	printf("Running tests for: '%s'\n", #test_suite_name);\
+	if(mtest_output_level == TIME){\
+		test_suite_name();\
+	} else{\
+		test_suite_name();\
+	}\
+	printf("Finished test suite\n");\
+} while (0)
 
+/* #define run_test(x) do { \
+	x;\
+} while (0)
+start_clock();
 
-// #define run_test(x) do { \
-// 	x;\
-// } while (0)
-// start_clock();
-
-    // stop_clock();
-    //TODO Clean up these two lines, they aren't nice
-
-#define MTEST_assert_int(assert,a,b) do {\
+    stop_clock();
+    TODO Clean up these two lines, they aren't nice
+*/
+/*#define MTEST_assert_int(assert,a,b) do {\
     if(((a==b) && (assert == TRUE)) || ((a !=b) && (assert == FALSE) )){\
         tests_passed++; \
         status =  1; \
@@ -69,7 +84,7 @@ int main(){
     tests_run++;\
     test_status(status);\
 } while (0)
-
+*/
 /*Private function declarations*/
 void test_status(int status);
 void start_clock();
@@ -84,26 +99,26 @@ void MTEST_INIT(void){
     tests_passed = 0;
 }
 
-// void MTEST_assert_int(int assert, int a, int b){
-//     start_clock();
-//     if(((a==b) && (assert == TRUE)) || ((a !=b) && (assert == FALSE) )){
-//         tests_passed++;
-//         status =  1;
-//     }    
-//     else{
-//         status = 0;
-//     }
+void MTEST_assert_int(int assert, int a, int b){
+    start_clock();
+    if(((a==b) && (assert == TRUE)) || ((a !=b) && (assert == FALSE) )){
+        tests_passed++;
+        status =  1;
+    }    
+    else{
+        status = 0;
+    }
 
-//     stop_clock();
-//     //TODO Clean up these two lines, they aren't nice
-//     tests_run++;
-//     test_status(status);
-// }
+    stop_clock();
+    //TODO Clean up these two lines, they aren't nice
+    tests_run++;
+    test_status(status);
+}
 
 void MTEST_INT_ARRAY(int assert, int *a, int *b){
     start_clock();
-    if(((memcmp(a, b, sizeof(a)) == 0 ) && (assert ==TRUE)) \
-        || ((memcmp(a, b, sizeof(a)) != 0) && (assert == FALSE))){
+    if(((memcmp(a, b, sizeof(&a)) == 0 ) && (assert ==TRUE)) \
+        || ((memcmp(a, b, sizeof(&a)) != 0) && (assert == FALSE))){
          tests_passed++;
          status = 1;
     }
@@ -137,8 +152,8 @@ void start_clock(void){
 }
 
 void stop_clock(){
-    clock_t end_t, total_t;
-    double seconds; 
+    clock_t end_t;
+    double seconds;
 
     end_t = clock();
 
--- mtest_template.c
+++ mtest_template.c
@@ -1,31 +1,39 @@
 #include "mtest.h"
 
 
-int test_function(void);
+// int test_function(void);
 
 
-int test_function(void)
-{
+int test_function(void){
     return 1;
 }
 
-int main(){
+void test_assert_function(void){
     int x = 1;
     int y = 1; 
+
+    MTEST_assert_int(TRUE, x, y);
+    y = 2;
+    MTEST_assert_int(TRUE, test_function(), y); //This should report as a fail 
+}
+
+void test_assert_array(void){
     int a[] = {1,2,3,4,5};
     int b[] = {1,2,3,4,5};
-    
-    /*This call initialises the behind-the-scene counters that provide
-     test feedback to the user */
-    MTEST_INIT();
-    
-    MTEST_assert_int(TRUE, x, y);
-
-    y = 2;
-    
-    MTEST_assert_int(TRUE, test_function(), y); //This should report as a fail 
 
     MTEST_INT_ARRAY(TRUE, a, b);
+
+}
+
+
+int main(){
+    
+    /*This call initialises the 'behind-the-scene' counters that provide
+     test feedback to the user */
+    MTEST_INIT();
+
+    MTEST_run_test_suite(test_assert_function,BASIC_RESULTS);
+    // MTEST_run_test_suite()
 
     MTEST_CLOSE();
 
