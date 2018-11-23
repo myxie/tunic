6f72d6db5f925d11427d314bebe7b0133cd1276b
Myxie <myxie@users.noreply.github.com>
2018-11-23 14:50:47 +0800

    Delete build.sh
    This is a library; people should be able to build it how they wish, this is just clutter.

--- build.sh
+++
@@ -1,7 +0,0 @@
-# Build file for the example test suite
-# Comment/Uncomment the lines for the compiler you are using (e.g. gcc, clang etc.) 
-
-gcc -std=c99 -Wall -Werror -pedantic -I=mtest.h mtest_template.c -o mtest
-
-clang -std=c99 -I=mtest.h mtest_template.c -o clmtest
-
